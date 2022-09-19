7fc558c7aa6536f9ed587c45ac80819e8bcc7175
Mohamed El Mahallawy <mmahalwy@gmail.com>
2022-09-19 15:32:04 -0400

    remove mantine

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
-
   render() {
     return (
       <Html>

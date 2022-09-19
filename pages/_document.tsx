473952118fd921a87cb9d5c43a26acc5a44dbaad
Mohamed El Mahallawy <mmahalwy@gmail.com>
2022-09-19 15:29:37 -0400

    remove mantine

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,4 @@
-import { createGetInitialProps } from '@mantine/next';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-
-const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
